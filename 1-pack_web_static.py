236a46165bfbae6e33a1e621234f2e287cce8dba
Ugwoke levi <113690452+levoski1@users.noreply.github.com>
2024-04-05 12:14:42 +0100

    great

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -16,6 +16,33 @@ def do_pack():
         local("mkdir -p ./versions")
         local("tar --create --verbose -z --file={} ./web_static"
               .format(file_name))
-        return file_name
+
+        # Check if the archive was created successfully
+        if local("test -e {}".format(file_name), capture=True).succeeded:
+            return file_name
     except:
         return None
+
+
+'''
+
+Function Execution:
+When you call the do_pack function in your Python script or Fabric task, it executes a series of steps to create a compressed archive (.tgz) of the contents of the web_static folder.
+
+Steps Performed:
+
+It first ensures that a folder named versions exists in the current directory.
+Then, it generates a timestamp using the current UTC time in the format %Y%m%d%H%M%S (Year, Month, Day, Hour, Minute, Second).
+Next, it constructs the filename for the archive by concatenating "versions/web_static_" with the generated timestamp and ".tgz".
+After that, it creates the archive using the tar command, compressing the contents of the web_static folder into the generated filename.
+Finally, it checks if the archive was successfully created by using the test -e <archive_name> command and capturing the output.
+Return Value:
+
+If the archive creation process succeeds, the function returns the path to the generated archive (e.g., "versions/web_static_<timestamp>.tgz").
+If any error occurs during the archive creation process, or if the archive does not exist after creation, the function returns None.
+Execution Context:
+
+The do_pack function runs in the local context, meaning it executes on the machine where the Fabric script is invoked.
+When you run the Fabric script, it executes locally on your development machine or wherever you initiate the script. The local function from Fabric executes shell commands on the local machine.
+'''
+
